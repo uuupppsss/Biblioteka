ee689a76b5a8eadb73a318dbf482cb27fde8f8b3
uuupppsss <ulya.ch.06@mail.ru>
2025-03-13 14:01:22 +1000

    sqlite

---
+++ BibliotekaAPI/Model/NotFakeDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BibliotekaAPI.Model
+{
+    public class NotFakeDB:DbContext
+    {
+        private readonly string _filename;
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public NotFakeDB(string filename)
+        {
+            _filename = filename;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var sqlitePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Database");
+            Directory.CreateDirectory(sqlitePath);
+            var filename = $"{sqlitePath}\f{_filename}";
+            if (!File.Exists(filename))
+                File.Create(filename);
+            optionsBuilder.UseSqlite($"Data Source={filename}");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
